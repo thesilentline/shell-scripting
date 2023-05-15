6ce4ab540ad140c025428aa2498a82b3c99aad84
PIIOT22DEC3012 <divyanshigsingh@gmail.com>
2023-05-15 15:06:58 +0530

    prints chess board of entered size

--- chess_board.sh
+++ chess_board.sh
@@ -1,12 +1,25 @@
 #/bin/bash/
+printf 'enter n \n'
+read a
 i=1
-t=4
-
-while [ "$i" -le "$t" ]
+current=0
+while [ "$i" -le "$a" ]
 do
-    
-    printf '\xe2\x96\x88 \xe2\x96\x88 \xe2\x96\x88 \xe2\x96\x88 \n'
-    printf ' \xe2\x96\x88 \xe2\x96\x88 \xe2\x96\x88 \xe2\x96\x88\n'
-   
+    t=$((a))
+    r=1
+    while [ "$r" -le "$t" ]
+    do
+        if [ "$current" -eq 0 ]
+        then
+            printf '\xe2\x96\x88'
+            current=1
+        else
+            printf ' '
+            current=0
+        fi
+        r=$((r+1))
+    done
+    current=$((!current))
+    printf '\n'
     i=$((i+1))
 done
